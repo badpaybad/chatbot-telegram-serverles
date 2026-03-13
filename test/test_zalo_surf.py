c85830ee878db9c50b117b04dd4962d12384e145
dunp <dunp@omt.vn>
2026-03-13 10:10:48 +0700

    zalo group read message

--- test/test_zalo_surf.py
+++ test/test_zalo_surf.py
@@ -1,7 +1,7 @@
 import sys
 import os
 # Thêm thư mục gốc vào sys.path để có thể import các module bot_telegram, config
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../')))
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../')))
 
 import asyncio
 from playwright.async_api import async_playwright
@@ -42,7 +42,7 @@ async def run_browser_agent():
 
         # Lấy username để tạo đường dẫn chính xác
         user_home = os.path.expanduser("~")
-        user_data_dir = f"{user_home}/.config/google-chrome/Zalo"
+        user_data_dir = f"{user_home}/.config/google-chrome/Default"
         print("user_data_dir",user_data_dir)
 
         # browser = await p.chromium.launch(channel="chrome", headless=False,
@@ -120,13 +120,11 @@ async def open_zalo_web():
 
     print("Đang mở trang đăng nhập...")
     await page.goto("https://chat.zalo.me/")
-    await page.wait_for_load_state("networkidle")
     await asyncio.sleep(3)
     pass
 
 
 async def download_qr_code():
-    await page.wait_for_load_state("networkidle")
     file_name="zalo_qr.png"
     # 1. Tìm selector: tìm img bên trong class .qr-container
     qr_selector = ".qr-container img"
@@ -142,7 +140,7 @@ async def download_qr_code():
             print("Không tìm thấy thuộc tính src của QR")
 
             try:
-                await page.get_by_text("Đã hiểu").click(force=True)    
+                await page.get_by_text("Đã hiểu").click()    
                 print(f"Click nút Đã hiểu") 
 
                 await asyncio.sleep(3)
@@ -176,71 +174,11 @@ async def download_qr_code():
 
         return None
 
-async def sync_zalo_chats_groups():
-    await page.wait_for_load_state("networkidle")
-    await asyncio.sleep(3)
-    try:
-        # z--btn--v2 btn-primary small sync-v2-ok suggestNewSync --rounded sync-v2-ok suggestNewSync
-        # await page.locator(".suggestNewSync.btn-primary", timeout=10000).click(force=True)
-
-        # Đợi cho tới khi DOM của trang mới thực sự sẵn sàng
-        await page.wait_for_selector(".sync-v2-banner.suggestNewSync", state="attached", timeout=10000)
-        await page.locator(".sync-v2-banner.suggestNewSync").click(force=True)
-
-    except Exception as e:
-        print(f"Không tìm thấy nút Đồng bộ ngay .suggestNewSync.btn-primary")
-    try:
-        # tds-conversation__footer-content-sync-button
-        await page.locator(".tds-conversation__footer-content-sync-button", timeout=10000).click(force=True)
-    except Exception as e:
-        print(f"Không tìm thấy nút Đồng bộ ngay .tds-conversation__footer-content-sync-button")
-
-    try:
-        await page.get_by_text("Nhấn để đồng bộ ngay", timeout=10000).click(force=True)    
-        print(f"Click nút Nhấn để đồng bộ ngay") 
-
-        await asyncio.sleep(3)
-    except Exception as e:
-        print(f"Không tìm thấy nút Nhấn để đồng bộ ngay")
-    try:
-        await page.get_by_text("Đồng bộ ngay", timeout=10000).click(force=True)    
-        print(f"Click nút Đồng bộ ngay") 
-
-        await asyncio.sleep(3)
-    except Exception as e:
-        print(f"Không tìm thấy nút Đồng bộ ngay")
-
-    try:
-        #sync-message-popup
-            
-        qr_selector = ".sync-message-popup"
-        while not isStop:
-            try:
-                found=await page.wait_for_selector(qr_selector, timeout=10000)
-                if found:
-                    print("Đang đồng bộ")
-                    await asyncio.sleep(10)
-
-                    print("Vẫn đang hiện .sync-message-popup")
-                    await bot_telegram.send_telegram_message(TELEGRAM_OWNER_USERID,f"Vẫn đang hiện .sync-message-popup, bạn mở zalo để đồng ý đồng bộ")
-                    await asyncio.sleep(10)
-            except Exception as ex:
-
-                print("Không có .sync-message-popup -> đã đồng bộ")
-                print(ex)
-                break 
-                
-                pass
-    except:
-        pass
-
 async def check_zalo_qr_auth():
-    await page.wait_for_load_state("networkidle")
 
     # https://id.zalo.me/account?continue=https%3A%2F%2Fchat.zalo.me%2F
 
-    # qr_file=await download_qr_code()
-    qr_file=None
+    qr_file=await download_qr_code()
 
     qr_selector = ".fa-Contact_28_Line"
     while not isStop:
@@ -248,34 +186,17 @@ async def check_zalo_qr_auth():
             found=await page.wait_for_selector(qr_selector, timeout=10000)
             if found:
                 print("QR code đã ẩn, đã vào trang chat")
-
                 return 
             else:
-                print(f"Redownload QR để check qr_selector: {qr_selector}")
                 if not qr_file:
                     qr_file=await download_qr_code()
-                    print("Redownload QR")
                 else:
                     print("QR code download xong, cần đăng nhập bằng điện thoại để vào trang chat")
                     await bot_telegram.send_telegram_message(TELEGRAM_OWNER_USERID,f"QR code download xong, cần đăng nhập bằng điện thoại để vào trang chat", 
                     files=[qr_file])
-                    await asyncio.sleep(5)      
                     
             await asyncio.sleep(3)               
-        except Exception as ex:
-            print(f"check_zalo_qr_auth Lỗi khi check QR: {ex}")
-            # case timeout dom selector
-
-            print(f"Redownload QR để check qr_selector: {qr_selector}")
-            if not qr_file:
-                qr_file=await download_qr_code()
-                print("Redownload QR")
-            else:
-                print("QR code download xong, cần đăng nhập bằng điện thoại để vào trang chat")
-                await bot_telegram.send_telegram_message(TELEGRAM_OWNER_USERID,f"QR code download xong, cần đăng nhập bằng điện thoại để vào trang chat", 
-                files=[qr_file])
-                await asyncio.sleep(5) 
-
+        except:
             await asyncio.sleep(3)
        
         pass
@@ -285,16 +206,13 @@ async def check_zalo_qr_auth():
     pass
 # vi du action queue, khi cần làm j đó với 1 page đã được mở lên, không tạo mới page ko sợ mất session         
 async def open_zalo_group_omt_tbp():
-    await page.wait_for_load_state("networkidle")
 
     print("click OMT-TBP")
-
     # page.locator("div:has-text('OMT-TBP')").click()
-   
-    await page.get_by_text("OMT-TBP").click(force=True, timeout=10000)
+    await asyncio.gather(
+        page.get_by_text("OMT-TBP").click()                
+    )
 
-    # await page.wait_for(state="visible", timeout=10000)
-    # await page.click(force=True)   
 
     text = await page.locator("div#messageViewContainer").text_content()
 
@@ -321,7 +239,6 @@ async def loop():
     
     browserActionQueue.put(open_zalo_web)
     browserActionQueue.put(check_zalo_qr_auth)
-    browserActionQueue.put(sync_zalo_chats_groups)
     browserActionQueue.put(open_zalo_group_omt_tbp)
 
     await asyncio.gather( run_browser_agent())
